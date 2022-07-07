5848bff7d20be5cd8d25b9cc3caf39f7cedcfdc1
christ <christikonga83@gmail.com>
2022-07-07 16:10:51 +0200

    tokens send mail

--- api/data/views.py
+++ api/data/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics,status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
 from .serializers import *
 class RegisterView(generics.GenericAPIView):
     serilazer_class = RegisterSerializer
@@ -10,6 +12,9 @@ class RegisterView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         user_data = serializer.data
+        
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user)
 
         return Response(user_data, status=status.HTTP_201_CREATED)
 
