636a0a28a3cf9f0cdabfb461621ada1784ed990a
christ <christikonga83@gmail.com>
2022-07-07 13:17:06 +0200

    registerView create and validate

--- api/data/views.py
+++ api/data/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
 from .serializers import *
 class RegisterView(generics.GenericAPIView):
     serilazer_class = RegisterSerializer
@@ -8,5 +9,8 @@ class RegisterView(generics.GenericAPIView):
         serializer = self.serilazer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        user_data = serializer.data
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
 
 # Create your views here.
