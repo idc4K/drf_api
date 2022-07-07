b8cb6d95a50b3c3f822d7e80c1f8dccecf6527b7
christ <christikonga83@gmail.com>
2022-07-07 12:47:34 +0200

    registerView

--- api/data/views.py
+++ api/data/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
 
+class RegisterView(generics.GenericAPIView):
+
+    def post(self,request):
+        user = request.data
 # Create your views here.
